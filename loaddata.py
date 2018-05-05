fb57fe042a41b626be88ec5a286b13e7233d1973
Floyd Luo <114020252@link.cuhk.edu.cn>
2018-05-05 14:03:55 +0800

    update loaddate

--- loaddata.py
+++ loaddata.py
@@ -5,7 +5,11 @@ import pandas as pd
 import numpy as np
 from splitresult import splitResult
 
-pklDictPath = 'pkldata/2017-11-03/CCT_Dict.p'
+from vector import cct2VecDF
+
+
+#filename = ['text']
+#types    = ['text']
 
 
 filename = ['01-一般项目/一般项目-',
@@ -17,85 +21,101 @@ types = [item.split('/')[-1].replace('-', '') for item in filename]
 
 def loadData(pklDictPath, seed):
     with open(pklDictPath, 'rb') as handle:
-        cctDict = pickle.load(handle)
+        cctDict_R = pickle.load(handle)
     
     cctTrain = []
     cctTest  = []
     n = 0
     for t in types:
         n+=1
-        typeCCT = cctDict[t]
+        typeCCT_R = cctDict_R[t]
         random.seed(n*seed)
-        l = random.sample(range(len(typeCCT)), int(len(typeCCT)*0.75))
+        l = random.sample(range(len(typeCCT_R)), int(len(typeCCT_R)*0.75))
         l.sort()
-        cctTypeTrain = [typeCCT[idx] for idx in l]
+        cctTypeTrain = [typeCCT_R[idx] for idx in l]
         cctTrain.append(cctTypeTrain)
-        cctTypeTest  = [typeCCT[idx] for idx in range(len(typeCCT)) if idx not in l]
+        cctTypeTest  = [typeCCT_R[idx] for idx in range(len(typeCCT_R)) if idx not in l]
         cctTest.append(cctTypeTest)
     return cctTrain, cctTest
 
-def getTrainData(cctTrain, cols= None, Path = None):
-    ## TODO
-    ## Too slow here.
-    for t in cctTrain:
-        for cct in t:
-            cct.getAnnotedEntities('RTag')
-            cct.coporAnnotation('RTag')
-            cct.getAnnotedEntities('ETag')
-            cct.coporAnnotation('ETag')
-            cct.outputCols = cols
+
+
+def getTrainData(cctTrain, attr_cols = None, tag_cols = None,  
+                 Path = None, 
+                 Vector = False, 
+                 vect_cols = None, 
+                 Vdim = None, 
+                 vect_path = None):
+
     DFtrain = pd.DataFrame()
     for t in cctTrain:
         for cct in t:
             df = cct.dfFormat.copy()
             df = df.dropna()
             df.loc[len(df)] = np.NaN     ## Trick Here
+            df = df[attr_cols + tag_cols]
+            
+            if Vector == True and vect_cols:
+                df_vec = cct2VecDF(cct, dim = Vdim, path = vect_path)
+                df_vec = df_vec.dropna()
+                df_vec.loc[len(df_vec)] = np.NaN
+                assert len(df_vec) == len(df)
+                df = pd.concat([df, df_vec], axis = 1)
+                df = df[attr_cols + vect_cols + tag_cols]
+                #print(df)
+                
             DFtrain = DFtrain.append(df) ## Trick Here
     DFtrain = DFtrain.reset_index(drop=True)
-    DFtrain.to_csv(Path, columns = cols, sep = '\t', encoding = 'utf=8', header = False, index = False )
+    # DFtrain = DFtrain[cols]
+    DFtrain.to_csv(Path, sep = '\t', encoding = 'utf=8', header = False, index = False )
+    # return DFtrain
 
-    
-def getTestData(cctTest, cols, Path):
+def getTestData(cctTest, attr_cols = None,  
+                Path = None,
+                Vector = False,
+                Vdim = None,
+                vect_cols = None,
+                vect_path = None):
+
     DFtest = pd.DataFrame()
     for t in cctTest:
         for cct in t:
-            cct.outputCols = cols
+            cct.outputCols = attr_cols
             df = cct.dfFormat.copy()
             df = df.dropna()
-            df.loc[len(df)] = np.NaN     ## Trick Here
+            df.loc[len(df)] = np.NaN    ## Trick Here
+            df = df[attr_cols]
+            
+            if Vector == True and vect_cols:
+                df_vec = cct2VecDF(cct, dim = Vdim, path = vect_path)
+                df_vec = df_vec.dropna()
+                df_vec.loc[len(df_vec)] = np.NaN
+                assert len(df_vec) == len(df)
+                df = pd.concat([df, df_vec], axis = 1)
+                df = df[attr_cols + vect_cols ]
+
             DFtest = DFtest.append(df)   ## Trick Here
+
     DFtest = DFtest.reset_index(drop=True)
-    DFtest.to_csv(Path, columns = cols, sep = '\t', encoding = 'utf=8', 
+    DFtest.to_csv(Path, sep = '\t', encoding = 'utf=8', 
                   header = False, index = False )
+    #return DFtest
 
-def getTestData2(cctTest, cols, Path, resultPath):
-    results = splitResult(resultPath)
-    cctTestList = sum(cctTest, [])
-    if len(cctTestList) != len(results):
-        return "Bad Performance, Not Match!!!"
-
-    DFtest = pd.DataFrame()
-    for idx in range(len(cctTestList)):
-        cct    = cctTestList[idx]
-        result = results[idx]
-        #cct.outputCols = cols 
-        cct.coporResult(result=result[1], newTag= 'RTag') ## TODO
-        cct.score = result[0]
-        #cct.getLearnedEntities('RTag')
-        cct.outputCols = cols
-        df = cct.dfFormat.copy()
-        df = df.dropna()
-        df.loc[len(df)] = np.NaN     ## Trick Here
-        DFtest = DFtest.append(df)   ## Trick Here
-    DFtest = DFtest.reset_index(drop=True)
-    DFtest.to_csv(Path, columns = cols, sep = '\t', encoding = 'utf=8', 
-                  header = False, index = False )
-
+def getTestData2(TempPath,
+                 Test2Path):
     
-
+    Test2Data = pd.read_csv(TempPath, 
+                            sep = '\t', 
+                            header = None, 
+                            skip_blank_lines=False)
+    Test2Data.to_csv(Test2Path, 
+                     sep = '\t', 
+                     encoding = 'utf=8', 
+                     header = False, 
+                     index = False )
 
 if __name__ == '__main__':
-    pklDictPath = 'pkldata/2017-11-03/CCT_Dict.p'
+    pklDictPath = 'pkldata/2017-11-03/R_CCT_Dict.p'
     btime = time.clock()
     cctTrain, cctTest = loadData(pklDictPath, 1)
     etime = time.clock()
@@ -103,25 +123,30 @@ if __name__ == '__main__':
     print([len(i) for i in cctTrain])
     print([len(i) for i in cctTest])
 
-    trainDataPara = {'cols': ['_atom', 'basicTag', 'dictTag', 'POSTag', 'ETag'],
+    DIM = 50
+    
+    trainDataPara = {'vect_cols': list(range(1,DIM + 1)),
+                     'Vector': True,
+                     'Vdim': 50,
+                     'attr_cols': ['_atom'],
+                     'tag_cols' : ['ETag'],
                      'Path':'demo/train.txt'}
 
     btime = time.clock()
-    df = getTrainData(cctTrain, **trainDataPara)
+    getTrainData(cctTrain, **trainDataPara)
     etime = time.clock()
     print(etime - btime)
 
-    testDataPara = {'cols': ['_atom', 'basicTag', 'dictTag', 'POSTag' ],
+    
+    testDataPara = {'attr_cols': ['_atom'],
+                    'vect_cols': list(range(1,DIM + 1)),
+                    'Vector': True,
+                    'Vdim': 50,
                     'Path':'demo/test.txt'}
 
     btime = time.clock()
     getTestData(cctTest, **testDataPara)
     etime = time.clock()
     print(etime - btime)
-
-
-
-
-
 
 
