f71b3afedcbe5e41ca3f739652c19ca4774428e4
Floyd Luo <114020252@link.cuhk.edu.cn>
2018-05-05 14:02:57 +0800

    update text

--- text.py
+++ text.py
@@ -1,16 +1,9 @@
-from pandas import read_csv
-from pandas import DataFrame as df
+
+import pandas as pd
 from jieba import posseg as ppseg
 import string
 
 from atom import Atom
-
-# this dictionary is used to deal with the annotated entities in texts.
-fLabel = {'症状和体征': 'Sy',
-          '身体部位':   'Bo',
-          '检查和检验': 'Ch',
-          '治疗':      'Tr',
-          '疾病和诊断': 'Si'}
 
 
 def strQ2B(ustring):
@@ -25,7 +18,6 @@ def strQ2B(ustring):
         rstring += chr(inside_code)
     return rstring
 
-
 def cleanSpace(text):
     #return text.replace("\t"," ").replace(" ","&nbsp;").replace('\n', '')
     '''
@@ -37,7 +29,6 @@ def cleanSpace(text):
     text =  text.replace('\n', '@')
     return text 
     
-
 def segmentStr(text):
     strLen = len(text)
     atoms = []
@@ -66,28 +57,19 @@ def getPOSTagsList(text):
                 
     return POSTagsList
 
-def getRTag(data):
+
+def getLearnedETag(data):
     if type(data) == str:
         return data.split('/')[0]
     else:
         return None
-
+    
 
 class ChineseClinicalText(object):
-    atoms = []
-    POSTags = None
-    outputCols =['_atom', 'basicTag']
-    newTags = []
-    entities = []
-    learnedEntities = []
-    annotedEntities = []
-    learnedREntities = []
-    annotedREntities = []
-    annotedFilePath = None
-    score = None
     
-    def __init__(self, text = None, originalFilePath = None, annotedFilePath = None):
+    def __init__(self, batch, text = None, originalFilePath = None, annotedFilePath = None):
         # to do: parse the text for this Chinise Clincal Text
+        self.batch = batch
         if text != None:
             self._text = cleanSpace(strQ2B(text))
             
@@ -114,7 +96,7 @@ class ChineseClinicalText(object):
                     self.annotedFilePath = annotedFilePath
             else:
                 print('Need a Text!!!')
-
+                
     def setAtomTags(self):
         self.atoms = []
         for i in range(len(self.atomsBaseList)):
@@ -123,25 +105,21 @@ class ChineseClinicalText(object):
             self.atoms.append(a)
             
     def getPOSTags(self):
-        # for Element is POSTags
-        # [index, char, POS-Tag]
-        # [8, '民', 'n-I']
         self.POSTags = getPOSTagsList(self._text)
-          
+        
     def setPOSTags(self):
         for a in self.atoms:
             a.POSTag = 'o'
             for pos in self.POSTags:
                 if a.index == pos[0] and a._atom == pos[1]:
                     a.POSTag = pos[-1]
-                    #print(a, '\t', a.POSTag)
-        
+    
+
     def updateDF(self):
-        'update the attribute for pandas PF form sentence'
-        DF = df([i.to_dict() for i in self.atoms])
-        # TODO, may not contain POS Tag
-        # OK, actually, it can. hh
-        self.dfFormat = DF[['_atom', 'basicTag', 'dictTag', 'radical', 'POSTag'] + self.newTags]
+        #update the attribute for pandas PF form sentence
+        DF = pd.DataFrame([i.to_dict() for i in self.atoms])
+        cols = ['_atom', 'basicTag', 'dictTag', 'radical', 'POSTag'] + self.newTags
+        self.dfFormat = DF[cols] # Here is to have a good order
         
     def toTextFile(self, filePath, cols = None):
         if cols != None:
@@ -149,15 +127,14 @@ class ChineseClinicalText(object):
         self.dfFormat.to_csv(filePath, columns = self.outputCols,
                              sep = '\t', encoding = 'utf=8', 
                              header = False, index = False )
-        
-    # For Test Data, need Copor Result Data
-    def coporResult(self, result, newTag):
-        # The requirements on result
-        # it is a dataframe
-        # exactly the same as the CCT
-        # include the right and same index of CCT's atoms
+
+
+       
+    ############ Deal With Learned E Tags, P1 ###########
+    def corpResult(self, result, newTag):
+        assert newTag == 'LearnedETag'
         result.columns = self.outputCols + ['Result'] + list(range((len(result.columns)-len(self.outputCols) -1)))
-        result[newTag] = result['Result'].apply(getRTag)
+        result[newTag] = result['Result'].apply(getLearnedETag)
         result = result[self.outputCols + [newTag]]
         for idx, row in result.iterrows():
             for a in self.atoms:
@@ -165,9 +142,50 @@ class ChineseClinicalText(object):
                     a.set_attr(newTag, row[newTag])
         self.newTags.append(newTag)
         self.updateDF()
+                     
+         
+    ############ Deal With Learned E Tags, P2 ###########
+    def getLearnedEntities(self, eType):
+        ''' eType could only be 'LearnedETag' '''
+        assert eType == 'LearnedETag'
+        taggedAtoms = [a for a in self.atoms if a.get_attr(eType) != 'O']
+        startIdx = [idx for idx in range(len(taggedAtoms)) if taggedAtoms[idx].get_attr(eType)[-2:] == '-B']
+        startIdx.append(len(taggedAtoms))
+        entitiesList = []
+        for i in range(len(startIdx)-1):
+            entityAtom = taggedAtoms[startIdx[i]: startIdx[i+1]]
+            startA, endA = entityAtom[0], entityAtom[-1]
+            entitiesList.append([''.join(ea._atom for ea in entityAtom), startA.index, endA.index, startA.get_attr(eType).split('-')[0]])
+        self.learnedEntities = [tuple(i) for i in entitiesList]
+        return entitiesList
+
+
+    ############ NEW: Deal With Annotated R and E Tags #####
+    def getAnnotedEntities(self, eType, annotedFilePath = None):
+        assert eType == 'ETag' or eType == 'RTag'
+        fLabel = self.batch['dataAnno']['fLabel']
+        sep =  self.batch['dataAnno']['sep']
+        start = self.batch['dataAnno']['start']
+        if annotedFilePath == None:
+            annotedFilePath = self.annotedFilePath
+        annotedEntities = []
+        with open(annotedFilePath, 'r') as f:
+            for l in f.readlines():
+                entity = strQ2B(l).replace('\n','').split(sep)
+                entity[-1] = fLabel[entity[-1]]
+                entity[1], entity[2] = int(entity[1])-start, int(entity[2])-start
+                annotedEntities.append(entity)
+        if eType == 'RTag':
+            for ae in annotedEntities:
+                ae[-1] = 'R'
+            self.annotedREntities = [tuple(i) for i in annotedEntities]
         
-    # For Train Data, need Copor Annotated Data
-    def coporAnnotation(self, newTag):
+        annotedEntities = [tuple(i) for i in annotedEntities]
+        self.annotedEntities = annotedEntities
+        return annotedEntities
+        
+    def corpAnnotation(self, newTag):
+        assert newTag == 'ETag' or newTag == 'RTag'
         entityTagsList = []
         if newTag == 'RTag':
             for ae in self.annotedREntities:
@@ -195,80 +213,25 @@ class ChineseClinicalText(object):
     
         self.newTags.append(newTag)
         self.updateDF()
-         
-    #### NEW
-    #### HERE
-    def getLearnedEntities(self, eType):
-        '''
-        eType = 'RTag' or 'ETag
-        Atoms have 'RTag' or 'ETag'
-        '''
-        taggedAtoms = [a for a in self.atoms if a.get_attr(eType) != 'O']
-        startIdx = [idx for idx in range(len(taggedAtoms)) if taggedAtoms[idx].get_attr(eType)[-2:] == '-B']
-        startIdx.append(len(taggedAtoms))
-        entitiesList = []
-        for i in range(len(startIdx)-1):
-            entityAtom = taggedAtoms[startIdx[i]: startIdx[i+1]]
-            startA, endA = entityAtom[0], entityAtom[-1]
-            entitiesList.append([''.join(ea._atom for ea in entityAtom), startA.index, endA.index, startA.get_attr(eType).split('-')[0]])
-        entitiesList = [tuple(i) for i in entitiesList]
-        if eType == 'RTag':
-            self.learnedREntities = entitiesList
-        if eType == 'ETag':
-            self.learnedEntities  = entitiesList
-        return entitiesList
-
-    def getAnnotedEntities(self, eType, annotedFilePath = None):
-        '''
-        eType = 'RTag' or 'ETag'
-        '''
-        if annotedFilePath == None:
-            annotedFilePath = self.annotedFilePath
-        annotedEntities = []
-        with open(annotedFilePath, 'r') as f:
-            for l in f.readlines():
-                entity = strQ2B(l).replace('\n','').split('\t')
-                entity[0] = cleanSpace(entity[0])
-                entity[-1] = fLabel[entity[-1]]
-                entity[1], entity[2] = int(entity[1]), int(entity[2])
-                annotedEntities.append(entity)
-
-        #New here, Add This to check the text is consistent or not
         
-        for ae in annotedEntities:
-            if self._text[ae[1]:ae[2]+1] != ae[0]:
-                print('Not Macthed:', self.originalFilePath)
-                break
         
-        #New here--END
-
-        if eType == 'RTag':
-            for ae in annotedEntities:
-                ae[-1] = 'R'
-            self.annotedREntities = [tuple(i) for i in annotedEntities]
-        elif eType == 'ETag':
-            self.annotedEntities  = [tuple(i) for i in annotedEntities]
-
-        return annotedEntities
-    
+    ############  Other Tools to Faciliate the Processes #########
     def data(self):
         s = {}
         # Num of Atoms
         s['Atoms'] = len(self.atoms)
-        s['learned_E'] = len(self.learnedEntities)
-        s['annoted_E'] = len(self.annotedEntities)
-        s['matched_E'] = len(set(self.learnedEntities).intersection(set(self.annotedEntities)))## Union vs Join
-        s['learned_R'] = len(self.learnedREntities)
-        s['annoted_R'] = len(self.annotedREntities)
-        s['matched_R'] = len(set(self.learnedREntities).intersection(set(self.annotedREntities)))## Union vs Join
-        entitiesLable = ['Sy','Bo', 'Ch', 'Tr', 'Si']
+        s['E_Learned'] = len(self.learnedEntities)
+        s['E_Annoted'] = len(self.annotedEntities)
+        s['E_Matched'] = len(set(self.learnedEntities).intersection(set(self.annotedEntities)))## Union vs Join
+        
+        entitiesLable = list(self.batch['dataAnno']['fLabel'].values())
         for eL in entitiesLable:
             elL = [e for e in self.learnedEntities if eL == e[-1]]
             elA = [e for e in self.annotedEntities if eL == e[-1]]
             elM = set(elA).intersection(set(elL)) ## Union vs Join
-            s[eL+'_learned'] = len(elL)
-            s[eL+'_annoted'] = len(elA)
-            s[eL+'_matched'] = len(elM)
+            s[eL+'_Learned'] = len(elL)
+            s[eL+'_Annoted'] = len(elA)
+            s[eL+'_Matched'] = len(elM)
         
         return s
     
@@ -277,6 +240,5 @@ class ChineseClinicalText(object):
         self.getPOSTags()    # generate atoms' POS tags. (only valid in text)
         self.setPOSTags()    # set atoms' POS tags
         self.updateDF()      # update atoms' attributes to DF format
-
-
-
+    
+    
